7de20efa04c34cd1ce9b24bf25f03f1864dea26f
Gabriela Aimée <princesinhadoriopardofilha@gmail.com>
2023-06-21 11:21:45 -0300

    21/06

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+def index(request):
+    # return HttpResponse('Olá, Django - index')
+    return render(request, 'index.html')
+
+def ola(request):
+    return HttpResponse('Olá, Django')
 
 # Create your views here.
+
+from django.http import HttpResponse
+
+    
