782541e577f24f873660a0821d285607042b9277
Gaby Aimée <princesinhadoriopardofilha@gmail.com>
2023-08-16 10:53:12 -0300

    novo

--- blog/views.py
+++ blog/views.py
@@ -8,3 +8,12 @@ def index(request):
 def ola(request):
     # return HttpResponse('Olá, Django');
     return render(request, 'home.html')
+
+
+from blog.models import Post # Acrescentar
+
+def ola(request): # Modificar
+    # return HttpResponse('Olá django')
+    posts = Post.objects.all() # recupera todos os posts do banco de dados
+    context = {'posts_list': posts } # cria um dicionário com os dado
+    return render(request, 'posts.html', context) # renderiza o template e passa o contexto
