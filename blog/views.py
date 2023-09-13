bb10b0333a32b2651632a255f645045b8baba09e
Gaby Aimée <princesinhadoriopardofilha@gmail.com>
2023-09-13 10:18:52 -0300

    Respostas JSON para o model Post

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
+import json
+from django.core.serializers.json import DjangoJSONEncoder
+
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 def index(request):
@@ -29,3 +32,32 @@ def ola(request): # Modificar
     posts = Post.objects.all() # recupera todos os posts do banco de dados
     context = {'posts_list': posts } # cria um dicionário com os dado
     return render(request, 'posts.html', context) # renderiza o template e passa o contexto
+
+def get_all_posts(request):
+    posts = list(Post.objects.values('pk', 'body_text', 'pub_date'))
+    data = {'success': True, 'posts': posts}
+    json_data = json.dumps(data, indent=1, cls=DjangoJSONEncoder)
+    response = HttpResponse(json_data, content_type='application/json')
+    response['Access-Control-Allow-Origin'] = '*' # requisição de qualquer origem
+    return response
+
+def get_post(request, post_id):
+    post = Post.objects.filter(
+        pk=post_id
+    ).values(
+        'pk', 'body_text', 'pub_date'
+    ).first()
+
+    data = {'success': True, 'post': post}
+    status = 200
+    if post is None:
+        data = {'success': False, 'error': 'Post ID não existe.'}
+        status=404
+        
+    response = HttpResponse(
+        json.dumps(data, indent=1, cls=DjangoJSONEncoder),
+        content_type="application/json",
+        status=status
+    )
+    response['Access-Control-Allow-Origin'] = '*' # requisição de qualquer origem
+    return response
